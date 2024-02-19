f7a664a11782cfbd8bacd21c46a465273d6a79ed
William George <williamgeorge1997x@gmail.com>
2024-02-20 00:44:36 +0200

    First commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') </title>
+    <link rel="stylesheet" href="{{ asset("css/all.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/bootstrap.min.css") }}">
+
+
+</head>
+<body>
+
+
+    <script src="{{ asset("js/bootstrap.bundle.min.js") }}"></script>
+
+</body>
+</html>
